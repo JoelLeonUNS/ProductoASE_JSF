a1eab5ae592b2fde8a6be92f99eee09db5254d49
AngieUNS <202014055@uns.edu.pe>
2023-08-11 03:45:50 -0500

    cambios

--- ProductoASE_JSF/src/main/java/Beans/BeanInforme.java
+++ ProductoASE_JSF/src/main/java/Beans/BeanInforme.java
@@ -1,27 +1,53 @@
 package Beans;
 
-import java.util.Arrays;
+import java.io.IOException;
+import java.io.Serializable;
 import java.util.List;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import PDFGenerator.PDFGenerator;
 import modelo.ModeloInforme;
+@ManagedBean
+@ViewScoped
 
-
-public class BeanInforme {
+public class BeanInforme implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private String opcionSeleccionada;
 	private ModeloInforme modeloInforme;
+	private PDFGenerator pdf;
+	private String nombreInforme;
 	
-    public List<String> getEncabezadosTabla() {
+    public BeanInforme() {
+        modeloInforme = new ModeloInforme();
+    }
+
+
+    public String[] getEncabezadosTabla() {
         if ("1".equals(opcionSeleccionada)) {
-            return Arrays.asList("Fecha", "Apellidos y Nombres","Edad","Genero","Diagnostico");
+            return new String[]{"Fecha", "Apellidos y Nombres", "Edad", "Genero", "Diagnostico"};
         } else if ("2".equals(opcionSeleccionada)) {
-            return Arrays.asList("Encabezado Columna ", "Encabezado Columna B");
+            return new String[]{"Facultad", "Escuela","Masculino","Femenino","Total"};
+        }else if ("3".equals(opcionSeleccionada)) {
+            return new String[]{"Paciente", "Condicion","Sexo","Cantidad","%"};
         }
         return null;
     }
+
+    
     public List<Object[]> getDatosTabla() {
         if ("1".equals(opcionSeleccionada)) {
             return modeloInforme.obtenerInforme1();
         } else if ("2".equals(opcionSeleccionada)) {
             return modeloInforme.obtenerInforme2();
+        }else if ("3".equals(opcionSeleccionada)) {
+            return modeloInforme.obtenerInforme3();
         }
         return null;
     }
@@ -37,5 +63,31 @@ public class BeanInforme {
 	public void setModeloInforme(ModeloInforme modeloInforme) {
 		this.modeloInforme = modeloInforme;
 	}
-    
+	
+	public String getNombreInforme() {
+		return nombreInforme;
+	}
+
+	public void setNombreInforme(String nombreInforme) {
+		this.nombreInforme = nombreInforme;
+	}
+	
+    public void generarInformePDF() {
+        try {
+            pdf.generarInformePDF(getDatosTabla(), getEncabezadosTabla(), nombreInforme);
+            mostrarMensaje("PDF generado exitosamente: " + nombreInforme + ".pdf");
+        } catch (IOException e) {
+            mostrarMensaje("Error al generar el PDF: " + e.getMessage());
+        }
+    }
+
+    private void mostrarMensaje(String mensaje) {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage(mensaje));
+    }
+
+
 }
+
+
+
